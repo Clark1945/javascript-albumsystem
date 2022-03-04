fa31cfed7d73e3d8802162b151ea39eb3260e712
Clark1945 <dmc950213@gmail.com>
2022-03-04 13:42:07 +0800

    加入新增,修改,刪除等功能

--- EX6/public/javascripts/album.js
+++ EX6/public/javascripts/album.js
@@ -42,3 +42,108 @@ function readURL(input){
         reader.readAsDataURL(input.files[0]);//讀取回傳(base64編碼)
     }
 }
+function initAlbum(data){
+    data.photos.forEach(function (img){
+        var commet = `<div style="width:300px;height:300px;float:left;margin:2%"
+        <a href="JavaScript:void(0)" onclick="selectImg('${img}',this)">
+        <img class="u_img"
+        src="/public/photos/${img}"/>
+        </a>
+        </div>`;
+        $('#albumForm').append(commet);
+    });
+}
+function getAlbum(){
+    if(!$.cookie('userID') || $.cookie('userID')=="null"){
+        alert("請先登入");
+        location.href='/public/login.html';
+        return;
+    }
+    $.post("/album/getAlbum",{'account':$.cookie('userID')},
+    function(res){
+        if(res.status==0){
+            initAlbum(res.data);
+        }
+    });
+}
+getAlbum();
+
+/*function selectImg(img,a){
+    $('#preview').attr('style','display:block');
+    $('#previewbg').attr('style','display:block');
+    $('#image').attr('src','/public/photos/'+img);
+}*/
+function closeDialog(){
+    $('#preview').attr('style','display:none');
+    $('#previewbg').attr('style','display:none');
+}
+var status="";
+var deleteImg=[];
+function onSelect(){
+    $("#selBtn").hide();
+    $("#delBtn").show();
+    $("#cnBtn").show();
+    status="onSelect";
+}
+function cnSelect(){
+    $("#selBtn").show();
+    $("#delBtn").hide();
+    $("#cnBtn").hide();
+    status="";
+    deleteImg=[];
+    $("u_img_file").removeClass("u_img_file");
+}
+function selectImg(img,a){
+    if(status=="onSelect"){
+        $(a).children().toggleClass("u_img_file");
+        if(deleteImg.indexOf(img)>-1){
+            deleteImg.splice(deleteImg.indexOf(img),1);
+        }
+        else{
+            deleteImg.push(img);
+        }
+    }else{
+        $('#preview').attr('style','display:block');
+        $('#previewbg').attr('style','display:block');
+        $('#image').attr('src','/public/photos/'+img);
+    }
+}
+function onDelete(){
+    if(!$.cookie('userID') || $.cookie('userID')=="null"){
+        alert("請先登入");
+        location.href='/public/login.html';
+        return;
+    }
+    if(deleteImg.length<1){
+        alert("尚未選取圖片");
+        return;
+    }
+    if(!confirm("確定刪除?")){
+        return;
+    }
+    else{
+        var url = "/album/delete";
+        var jsondata = JSON.stringify({
+            "account":$.cookie("userID"),
+            "images":deleteImg
+        });
+        $.ajax({
+            url:url,
+            type:"POST",
+            data:jsondata,
+            contentType:"application/json",
+            success: function (res){
+                if(res.status==0){
+                    alert("刪除成功");
+                    history.go(0);
+                }
+                else{
+                    alert(res.status);
+                }
+            },
+            error: function (err){
+                console.log(err);
+            }           
+        })
+    }
+}
--- EX6/routes/albumApi.js
+++ EX6/routes/albumApi.js
@@ -1,10 +1,9 @@
-const { storage } = require('debug/src/browser');  //自動加入
 var express=require('express');
 var router=express.Router();
 var multer=require('multer');
 var memberModel=require('../models/memberModel');
 
-var sotrage=multer.diskStorage({
+var storage=multer.diskStorage({
     destination: function (req,file,cb){  //cb callback方法
         cb(null,'./public/photos');//儲存地，error不進行動作
     },
@@ -31,5 +30,42 @@ router.post("/upload",upload.single("file"),function(req,res,next){
 });
 
 
+router.post("/getAlbum",function(req,res,next){
+    memberModel.findOne({account:req.body.account},function(err,data){
+        if(err){
+            res.json({'status':1,'msg':'error'});
+        }
+        else{
+            res.json({'status':0,'msg':'success','data':data});
+        }
+    });
+});
+
+
+router.post("/delete",function(req,res,next){
+    memberModel.findOne({account:req.body.account},function(err,data){
+        if(err){
+            res.json({'status':1,'msg':'error'});
+        }
+        else{
+            var images=req.body.images;
+            for(var i in images){
+                var index=data.photos.indexOf(images[i]);
+                if(index>-1){
+                    data.photos.splice(index,1);
+                }
+            }
+            //data.markModified("photos");
+            data.save(function(err){
+                if(err){
+                    res.json({'status':1,"msg":'error'});
+                }
+                else{
+                    res.json({'status':0,'msg':'success','photos':data.photos});
+                }
+            });
+        }
+    });
+});
 
 module.exports=router;
